e9586472304de42fb469fc98905fa46787a5f76f
4ervenkov749s <dimitar_87@abv.bg>
2022-02-06 11:55:56 +0200

    add host project

--- MotoShop/Program.cs
+++ MotoShop/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
 
 namespace MotoShop
 {
@@ -13,11 +9,29 @@ namespace MotoShop
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application start-up failed!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- MotoShop/Startup.cs
+++ MotoShop/Startup.cs
@@ -1,13 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MotoShop.BL.Interfaces;
+using MotoShop.BL.Services;
+using MotoShop.DL.Interfaces;
+using MotoShop.DL.Repositories;
+using MotoShop.Extensions;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Serilog;
 
 namespace MotoShop
 {
@@ -23,25 +26,56 @@ namespace MotoShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+
+            services.AddSingleton(Log.Logger);
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddSingleton<IMotoRepository, MotoInMemoryRepository>();
+            services.AddSingleton<IClientRepository, ClientInMemoryRepository>();
+            services.AddSingleton<IEmployeeRepository, EmployeeInMemoryRepository>();
+            services.AddSingleton<IPartRepository, PartInMemoryRepository>();
+            services.AddSingleton<IServiceRepository, ServiceInMemoryRepository>();
+
+            services.AddSingleton<IMotoService, MotoService>();
+            services.AddSingleton<IClientService, ClientService>();
+            services.AddSingleton<IEmployeeService, EmployeeService>();
+            services.AddSingleton<IPartService, PartService>();
+            services.AddSingleton<IServiceService, ServiceService>();
+
+            services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MotoShop", Version = "v1" });
+            });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MotoShop v1"));
             }
 
+
+
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
+            app.ConfigureExceptionHandler(logger);
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +83,9 @@ namespace MotoShop
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                //endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
